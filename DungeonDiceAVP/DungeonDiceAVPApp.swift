023c94f35edd56845361ae4223333844b5d17651
Bob Witmer <bwitmer@golden.net>
2025-08-15 10:02:19 -0400

    Initial Commit

---
+++ DungeonDiceAVP/DungeonDiceAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceAVPApp.swift
+//  DungeonDiceAVP
+//
+//  Created by Bob Witmer on 2025-08-15.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
