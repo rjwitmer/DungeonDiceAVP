317ab997b5f4e04b868c7be7afe4b2d5eaf35142
Bob Witmer <bwitmer@golden.net>
2025-08-17 10:03:19 -0400

    Completed Chapter 4.6

--- DungeonDiceAVP/ContentView.swift
+++ DungeonDiceAVP/ContentView.swift
@@ -11,10 +11,14 @@ import RealityKitContent
 
 struct ContentView: View {
     @State private var resultMessage: String = ""
+    @State private var imageName: String = ""
     @State private var imageHidden: Bool = true
+    @State private var textHidden: Bool = true
+    @State private var animationTrigger: Bool = false
+    @State private var isDoneAnimating: Bool = true
     
     enum Dice: Int, CaseIterable, Identifiable {
-
+        
         
         case four = 4
         case six = 6
@@ -39,35 +43,49 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-
+            
             Text("Dungeon Dice")
                 .font(.extraLargeTitle)
                 .fontWeight(.black)
                 .foregroundColor(.red)
-
+                  
             Spacer()
             
-            if !imageHidden{
-                Image(systemName: "dice")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .foregroundStyle(Color.red)
-            }
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(isDoneAnimating ? 1 : 0.6) // Animate between 60% and 100% size
+                .opacity(isDoneAnimating ? 1 : 0.2)     // Set Opacity of Animation between 20% and 100%
+                .frame(width: 100, height: 100)
+                .foregroundStyle(Color.red)
+                .onChange(of: animationTrigger) {
+                    isDoneAnimating = false // set to the beginnning "false" state
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.2)) {
+                        isDoneAnimating = true
+                    }
+                }
             
             Text(resultMessage)
                 .font(.title)
                 .fontWeight(.medium)
-            
-
-            
+//                .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (x: 1, y: 0, z: 0))
+//                .onChange(of: animationTrigger) {
+//                    isDoneAnimating = false // set to the beginnning "false" state
+//                    withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
+//                        isDoneAnimating = true
+//                    }
+//                }
+             
             Spacer()
-
+            
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 172))], spacing: 60) {
                 ForEach(Dice.allCases) { die in
                     Button(die.description) {
                         resultMessage = "You rolled a \(die.roll()) on a \(die)-sided die!"
+                        imageName = "dice"
                         imageHidden = false
+                        animationTrigger.toggle()
+                        textHidden = false
                     }
                 }
             }
